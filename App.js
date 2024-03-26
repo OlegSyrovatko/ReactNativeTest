0e3533dec5e01e3f6a21c56b6a5e3b372f57712a
Oleg <sirov@ukr.net>
2024-03-27 00:41:29 +0200

    add touched

--- App.js
+++ App.js
@@ -1,89 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Alert,
-  Text,
-  View,
-  FlatList,
-  ActivityIndicator,
-} from "react-native";
-import { Post } from "./components/Post";
-import axios from "axios";
 import React from "react";
+import { HomeScreen } from "./screens/Home";
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
 
 export default function App() {
-  const [isLoading, setisLoading] = React.useState(true);
-  const [items, setItems] = React.useState();
-
-  const fetchPosts = () => {
-    setisLoading(true);
-    axios
-      .get("https://6477a5629233e82dd53bfca6.mockapi.io/posts")
-      .then(({ data }) => {
-        setItems(data);
-      })
-      .catch((err) => {
-        console.log(err);
-        Alert.alert("my error", "Can not get news");
-      })
-      .finally(() => {
-        setisLoading(false);
-      });
-  };
-
-  React.useEffect(fetchPosts, []);
-
-  if (isLoading) {
-    return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <ActivityIndicator size="large" />
-        <Text
-          style={{
-            marginTop: 15,
-          }}
-        >
-          Loading...
-        </Text>
-      </View>
-    );
-  }
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={items}
-        renderItem={({ item }) => (
-          <Post
-            title={item.title}
-            description={item.description}
-            avatar={item.avatar}
-            createdAt={item.createdAt}
-          />
-        )}
-      />
-
-      <Text style={styles.testText}>Open up App.js</Text>
-      <Text style={{ color: "green" }}>start wor king on your app!!</Text>
+    <View>
+      <HomeScreen />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 40,
-    // flex: 1,
-    backgroundColor: "lightyellow",
-    // alignItems: "center",
-    // justifyContent: "center",
-  },
-  testText: {
-    color: "blue",
-    fontSize: 24,
-  },
-});
---
+++ screens/Home.jsx
@@ -0,0 +1,95 @@
+import {
+  StyleSheet,
+  Alert,
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator,
+  RefreshControl,
+  TouchableOpacity,
+} from "react-native";
+import { Post } from "../components/Post";
+import axios from "axios";
+import React from "react";
+
+export const HomeScreen = () => {
+  const [isLoading, setisLoading] = React.useState(true);
+  const [items, setItems] = React.useState();
+
+  const fetchPosts = () => {
+    setisLoading(true);
+    axios
+      .get("https://6477a5629233e82dd53bfca6.mockapi.io/posts")
+      .then(({ data }) => {
+        setItems(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        Alert.alert("my error", "Can not get news");
+      })
+      .finally(() => {
+        setisLoading(false);
+      });
+  };
+
+  React.useEffect(fetchPosts, []);
+
+  if (isLoading) {
+    return (
+      <View
+        style={{
+          marginTop: 300,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ActivityIndicator size="large" />
+        <Text
+          style={{
+            marginTop: 15,
+          }}
+        >
+          Loading...
+        </Text>
+      </View>
+    );
+  }
+  return (
+    <View style={styles.container}>
+      <FlatList
+        refreshControl={
+          <RefreshControl refreshing={isLoading} onRefresh={fetchPosts} />
+        }
+        data={items}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <Post
+              title={item.title}
+              description={item.description}
+              avatar={item.avatar}
+              createdAt={item.createdAt}
+            />
+          </TouchableOpacity>
+        )}
+      />
+
+      <Text style={styles.testText}>Open up App.js</Text>
+      <Text style={{ color: "green" }}>start wor king on your app!!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    // flex: 1,
+    backgroundColor: "lightyellow",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  testText: {
+    color: "blue",
+    fontSize: 24,
+  },
+});
