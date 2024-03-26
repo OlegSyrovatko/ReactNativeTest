3980a94b01f4a4253a487638e6037713cd280430
Oleg <sirov@ukr.net>
2024-03-26 23:47:54 +0200

    add Loading

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Alert, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Alert,
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 import { Post } from "./components/Post";
 import axios from "axios";
 import React from "react";
 
 export default function App() {
+  const [isLoading, setisLoading] = React.useState(true);
   const [items, setItems] = React.useState();
-  React.useEffect(() => {
+
+  const fetchPosts = () => {
+    setisLoading(true);
     axios
       .get("https://6477a5629233e82dd53bfca6.mockapi.io/posts")
       .then(({ data }) => {
@@ -15,19 +25,47 @@ export default function App() {
       .catch((err) => {
         console.log(err);
         Alert.alert("my error", "Can not get news");
+      })
+      .finally(() => {
+        setisLoading(false);
       });
-  }, []);
+  };
+
+  React.useEffect(fetchPosts, []);
+
+  if (isLoading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ActivityIndicator size="large" />
+        <Text
+          style={{
+            marginTop: 15,
+          }}
+        >
+          Loading...
+        </Text>
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
-      {items.map((obj) => (
-        <Post
-          key={obj.id}
-          title={obj.title}
-          description={obj.description}
-          avatar={obj.avatar}
-          createdAt={obj.createdAt}
-        />
-      ))}
+      <FlatList
+        data={items}
+        renderItem={({ item }) => (
+          <Post
+            title={item.title}
+            description={item.description}
+            avatar={item.avatar}
+            createdAt={item.createdAt}
+          />
+        )}
+      />
 
       <Text style={styles.testText}>Open up App.js</Text>
       <Text style={{ color: "green" }}>start wor king on your app!!</Text>
